70db07a9b05dce9442b8e10f1e79fed07a18b273
Jan Wirth <jan@scalab.app>
2020-03-07 22:17:50 +0100

    Complete bandcamp e2e download

--- src/bandcamp.js
+++ src/bandcamp.js
@@ -1,7 +1,9 @@
+// A very illegal and hacky adapter for bandcamp
 const fetch = require('isomorphic-fetch')
 const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities();
 const fs = require("fs")
+const Downloader = require("./downloader")
 
 // extract the data encoded in the bandcamp data blob
 const fetchAndSlice = async (cookie, url) => {
@@ -15,25 +17,47 @@ const fetchAndSlice = async (cookie, url) => {
                 }
             )
       const text = await data.text()
-      // extract data
-      const re = /data-blob="(\{.*})"/
-      const blobAndTail = text.split("data-blob=\"")[1]
-      const blob = blobAndTail.split("\"></div>")[0]
-      return JSON.parse(entities.decode(blob))
+      return slice(text)
+}
+const slice = text => {
+    // extract data
+    const blobAndTail = text.split("data-blob=\"")[1]
+    const blob = blobAndTail.split("\"></div>")[0]
+    return JSON.parse(entities.decode(blob))
 }
 
 const connect = app => async cookie => {
-    console.log("bandcamp init called")
   const initData = await fetchAndSlice(cookie, "https://bandcamp.com")
   // @@ TODO FIX for some reason the part after await is called twice
   const username = initData.identities.fan.username
-  // console.log(username)
   const collectionData = await fetchAndSlice(cookie, "https://bandcamp.com/" + username)
-  console.log(initData)
-  console.log(collectionData)
   app.ports.bandcamp_library_retrieved.send(collectionData)
 }
+
+// fetch the asset from bandcamp servers and unzip
 const download = app => async ([cookie, purchase_id, download_url]) => {
-    console.log(cookie, purchase_id, download_url)
+    if (!Downloader.already_downloaded(purchase_id)) {
+        const data = await fetchAndSlice(cookie, download_url)
+        const mp3Download = data.download_items[0].downloads['mp3-v0']
+        const asset_url = await get_asset_url(cookie, mp3Download.url)
+        Downloader
+            .with_progress(purchase_id, asset_url, state => console.log('got it', state))
+    } else {
+        Downloader
+            .unzip(purchase_id)
+    }
 }
+// fetch the asset URl from bandcamp
+const get_asset_url = async (cookie, encode_url) => {
+      const {url} =
+        await fetch(encode_url,
+                { method: 'GET'
+                , headers: {Cookie: cookie}
+                }
+            )
+      return url
+    }
+
+
+
 module.exports = {connect, download}
