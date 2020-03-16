75179e12888c3c8925d9f6949cfe13beb9a7c174
Jan Wirth <jan@scalab.app>
2020-03-16 11:06:00 +0100

    Clean up browser-main

--- src/browser-main.js
+++ src/browser-main.js
@@ -4,27 +4,30 @@ const FileSystem = require("./FileSystem")
 const {register} = require("./custom-elements")
 const {Elm} = require("../elm-stuff/elm.js")
 
-
-
 register()
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
 
+  // log environment
+  console.log(`${type}-version`, process.versions[type])
 
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+  // monkey patch file drop API
   window.DataTransferItem.prototype.__defineGetter__("entry", function() {
       const entry = this.webkitGetAsEntry()
       return entry;
   });
+
+  // init and connect persistence layer
+  const seed = makeSeed()
+  const restored = Storage.restore()
+  const flags = {restored, ...seed}
+  const app = Elm.Main.init({flags})
+  app.ports.persist_.subscribe(Storage.persist)
+  // connect file system access
+  app.ports.scan_paths.subscribe(FileSystem.import_(app))
+  Bandcamp.setupPorts(app)
 })
 
 // makeSeed : () => string
@@ -35,16 +38,3 @@ const makeSeed = () => {
     const [seed, ...seed_extension] = generated_seed
     return {seed, seed_extension}
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    // init and connect persistence layer
-    const seed = makeSeed()
-    const restored = Storage.restore()
-    const flags = {restored, ...seed}
-    const app = Elm.Main.init({flags})
-    app.ports.persist_.subscribe(Storage.persist)
-    // connect file system access
-    app.ports.scan_paths.subscribe(FileSystem.import_(app))
-    Bandcamp.setupPorts(app)
-})
