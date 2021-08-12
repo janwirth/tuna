9c1fce380bd2e0bb06e67e11410bcf98824315c1
Jan Wirth <jan@funklang.com>
2021-08-12 12:51:11 +0200

    Add rodio example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,47 @@
 use iced::{button, Align, Button, Column, Element, Sandbox, Settings, Text};
 
-pub fn main() -> iced::Result {
-    Counter::run(Settings::default())
+use std::fs::File;
+use std::fs;
+use std::path;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source, Sink};
+
+pub fn main() {
+
+    let paths = fs::read_dir("music").unwrap();
+    for path in paths {
+        let p = path.unwrap().path();
+
+        println!("Name: {}", p.display());
+        play_track(p)
+    }
 }
+
+fn play_track(track: path::PathBuf) {
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open(track).unwrap());
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+    // Play the sound directly on the device
+    // stream_handle.play_raw(source.convert_samples());
+
+    // The sound plays in a separate audio thread,
+    // so we need to keep the main thread alive while it's playing.
+    // std::thread::sleep(std::time::Duration::from_secs(5));
+
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    sink.append(source);
+
+    // The sound plays in a separate thread. This call will block the current thread until the sink
+    // has finished playing all its queued sounds.
+    sink.sleep_until_end();
+}
+
+// pub fn main() -> iced::Result {
+//     Counter::run(Settings::default())
+// }
 
 #[derive(Default)]
 struct Counter {
